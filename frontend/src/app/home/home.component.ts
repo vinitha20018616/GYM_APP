9ea974bbc6fd40076909db55346de2268a35225d
vinitha20018616 <subashvinithamary@gmail.com>
2024-12-02 16:21:52 +0000

    home page added

---
+++ frontend/src/app/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  // You can add any component logic here if needed
+}
